7628c99cacc4629bdc075c05e4d0d8e3db9e990b
Chidindu Nwaroh <chidoskime101@gmail.com>
2024-08-23 23:09:11 +0100

    Added new feature

--- js/index.js
+++ js/index.js
@@ -1,15 +1,43 @@
 var backToTopButton = document.getElementById("back-to-top");
 
-// When the user scrolls down 200px from the top of the document, show the button
-window.onscroll = function() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
 };
 
 // When the user clicks on the button, scroll to the top of the document
-backToTopButton.onclick = function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+backToTopButton.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".fade-in-section");
+  const unitSections = document.querySelectorAll(".fade-in-units");
+
+  const options = {
+    root: null, // relative to the viewport
+    threshold: 0.15, // trigger when 15% of the section is visible
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible-faded");
+        observer.unobserve(entry.target); // Stop observing after visible
+      }
+    });
+  }, options);
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+  unitSections.forEach((unitSection) => {
+    observer.observe(unitSection);
+  });
+});
